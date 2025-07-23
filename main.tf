d8e6b03112d5b71cb1285526c0ad967bf7de63c6
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2025-07-23 17:41:57 +0700

    Add VPC module and update EC2 instance configuration
    - Added a new module for VPC creation with specified CIDR and subnets.
    - Updated the EC2 instance resource to reference the new VPC's security group and private subnet.
    - Included a data source to fetch the latest Ubuntu AMI.

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# Tìm AMI Ubuntu mới nhất
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -13,9 +14,29 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# Thêm module VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
+}
+
+# EC2 instance nằm trong VPC mới
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+  # Tham chiếu VPC
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
     Name = var.instance_name
